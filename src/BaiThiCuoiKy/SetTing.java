617fcde8d68b1fdd09a04fc19a0bece61fe7a8b5
Phanphuoc558 <phanphuocpro430@gmail.com>
2021-12-08 11:42:37 +0700

    e hông dùng ArrayList thầy cho e điểm cao cao xí nha <3

--- src/BaiThiCuoiKy/SetTing.java
+++ src/BaiThiCuoiKy/SetTing.java
@@ -4,29 +4,29 @@ import java.text.ParseException;
 
 public class SetTing {
     public static void settingName(HangHoa hangHoa){
-        System.out.println("Tên ban đầu: "+hangHoa.getName());
-        System.out.print("Nhập tên mới <3: ");
+        System.out.println("Ten ban dau: "+hangHoa.getName());
+        System.out.print("Nhap ten moi <3: ");
         hangHoa.setName(InputTools.inputString());
         System.out.println("Name has been change!");
         Creen.clearEnter();
     }
     public static void settingImportPrice(HangHoa hangHoa){
-        System.out.println("Giá nhập ban đầu: "+hangHoa.getImportPrice());
-        System.out.println("Nhập giá mới <3: ");
+        System.out.println("Gia nhap ban dau: "+hangHoa.getImportPrice());
+        System.out.println("Nhap gia moi <3: ");
         hangHoa.setImportPrice(Float.parseFloat(InputTools.inputString()));
         System.out.println("Import Price has been change!");
         Creen.clearEnter();
     }
     public static void settingInventoryNumber(HangHoa hangHoa){
-        System.out.println("Số lượng tồn kho hiện tại: "+hangHoa.getInventoryNumber());
-        System.out.println("Tồn kho mới <3: ");
+        System.out.println("So luong ton kho hien tai: "+hangHoa.getInventoryNumber());
+        System.out.println("Ton kho moi <3: ");
         hangHoa.setInventoryNumber(Integer.parseInt(InputTools.inputString()));
         System.out.println("Inventory Number has been change!");
         Creen.clearEnter();
     }
     public static void settingDate(HangHoa hangHoa) throws ParseException{
-        System.out.println("Ngày nhập ban đầu: "+FormatDate.dateToString(hangHoa.getInputDate()));
-        System.out.print("Nhập vào ngày mới: ");
+        System.out.println("Ngay nhap ban dau: "+FormatDate.dateToString(hangHoa.getInputDate()));
+        System.out.print("Nhap vao ngay moi: ");
         hangHoa.setInputDate(InputTools.inputDate());
         System.out.println("Input Date has been change!");
         Creen.clearEnter();
